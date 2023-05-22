f574c1e96b3d9172f12689a82b9d54948f20dc18
Bahaa Alsharif <bhlshrf@gmail.com>
2023-05-22 15:05:52 +0300

    13 Technical setup (#20)
    * Add docker compose with Ruuter, Resql, Liquibase and DataMapper
    
    * Add GUI dummy code
    
    * Dummy end-to-end example
    
    * Add TIM service
    
    * Ignore tim-db folder
    
    * Add empty line

---
+++ GUI/src/pages/OverviewPage.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useTranslation } from "react-i18next";
+import { Button, Track } from "../components";
+import { dummyApi, toggleDummyApi } from "../resources/api-constants";
+
+const OverviewPage: React.FC = () => {
+  const { t } = useTranslation();
+  const [result, setResult] = useState('')
+
+  useEffect(() => {
+    fetchDummy()
+  }, [])
+
+  const fetchDummy = async () => {
+    try {
+      setResult('loading . . . .');
+      const res = await axios.get(dummyApi());
+      setResult(JSON.stringify(res.data));
+    } catch (err: any) {
+      setResult('failed to fetch: ' + err.toString());
+    }
+  }
+
+  const handleToggleClick = async () => {
+    try {
+      setResult('loading . . . .');
+      await axios.post(toggleDummyApi());
+      await fetchDummy();
+    } catch (err: any) {
+      setResult('failed to toggle: ' + err.toString());
+    }
+  }
+
+  return (
+    <>
+      <Track justify="between">
+        <h1>{t("overview.title")}</h1>
+      </Track>
+
+      <Track gap={20}>
+        <span>
+          {result}
+        </span>
+        <Button onClick={handleToggleClick}>toggle</Button>
+      </Track>
+    </>
+  );
+};
+
+export default OverviewPage;
