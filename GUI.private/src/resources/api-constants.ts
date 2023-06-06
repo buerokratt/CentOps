f6fa39c01040d545fab83eace05ab5719d6f5401
Karl Jõgi <32520242+karljogi@users.noreply.github.com>
2023-06-06 11:21:25 +0300

    Fix API path for invitation

--- GUI.private/src/resources/api-constants.ts
+++ GUI.private/src/resources/api-constants.ts
@@ -2,6 +2,7 @@ const baseUrl = process.env.REACT_APP_API_URL;
 
 export const dummyApi = (): string => `${baseUrl}/dummy`;
 export const toggleDummyApi = (): string => `${baseUrl}/toggle-dummy`;
-export const createInvitation = (): string => `${baseUrl}/invitation`;
+export const createInvitation = (): string =>
+  `${baseUrl}/admin/participants/invitation`;
 export const getInstitutionsRequests = (): string =>
   `${baseUrl}/institutions-requests`;
