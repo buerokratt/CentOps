98eacb02495ff7ac6ad041733a7168ca5bd31270
baha-a <bhlshrf@gmail.com>
2023-06-14 02:13:38 +0300

    Add custom form working example

--- GUI.public/src/components/CustomForm/index.tsx
+++ GUI.public/src/components/CustomForm/index.tsx
@@ -12,7 +12,7 @@ interface Field {
 }
 
 interface FormData {
-  version: string;
+  title: string;
   vaildatorSource: string;
   fields: Field[];
 }
@@ -20,9 +20,14 @@ interface FormData {
 interface CustomFormProps {
   formId: string;
   onSubmit: (formData: any) => void;
+  hideTitle?: boolean;
 }
 
-const CustomForm: FC<CustomFormProps> = ({ formId, onSubmit }: CustomFormProps) => {
+const CustomForm: FC<CustomFormProps> = ({
+  formId,
+  onSubmit,
+  hideTitle,
+}: CustomFormProps) => {
   const [formData, setFormData] = useState<FormData | null>(null);
   const [error, setError] = useState<boolean>(false);
   const [formValues, setFormValues] = useState<{ [key: string]: string }>({});
@@ -41,20 +46,20 @@ const CustomForm: FC<CustomFormProps> = ({ formId, onSubmit }: CustomFormProps) 
     }
   }
 
-  const handleSubmit = (e: FormEvent) => {
-    e.preventDefault();
-    onSubmit({ formId, values: formValues });
-  }
-
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
     let { name, value } = e.target;
     if (e.target.type === 'checkbox') {
-      value = value === 'on' ? 'off' : 'on';
+      value = formValues[name] === 'on' ? 'off' : 'on';
     }
     setFormValues((prevState) => ({
       ...prevState,
       [name]: value,
     }));
+  }
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    onSubmit({ formId, values: formValues });
   }
 
   if (error) {
@@ -74,12 +79,15 @@ const CustomForm: FC<CustomFormProps> = ({ formId, onSubmit }: CustomFormProps) 
 
   return (
     <div>
+      {!hideTitle && <h4>{formData.title}</h4>}
       <form onSubmit={handleSubmit}>
         {formData.fields.map((field: any) => {
           switch (field.type) {
             case 'text':
             case 'email':
             case 'number':
+            case 'date':
+            case 'time':
               return (
                 <div key={field.id}>
                   <label htmlFor={field.id}>{field.label}</label>
@@ -101,19 +109,6 @@ const CustomForm: FC<CustomFormProps> = ({ formId, onSubmit }: CustomFormProps) 
                     id={field.id}
                     name={field.id}
                     checked={formValues[field.id] === 'on'}
-                    onChange={handleInputChange}
-                  />
-                </div>
-              );
-            case 'date':
-              return (
-                <div key={field.id}>
-                  <label htmlFor={field.id}>{field.label}</label>
-                  <input
-                    type="date"
-                    id={field.id}
-                    name={field.id}
-                    value={formValues[field.id] || ''}
                     onChange={handleInputChange}
                   />
                 </div>
--- GUI.public/src/pages/OverviewPage.tsx
+++ GUI.public/src/pages/OverviewPage.tsx
@@ -1,8 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { Button, Track } from '../components';
-import { dummyApi, toggleDummyApi } from '../resources/api-constants';
+import { Track } from '../components';
 import CustomForm from '../components/CustomForm';
 
 const OverviewPage: React.FC = () => {
@@ -13,7 +11,7 @@ const OverviewPage: React.FC = () => {
       <Track justify="between">
         <h1>{t('overview.title')}</h1>
       </Track>
-      <CustomForm formId='1234' onSubmit={(data) => console.log(data)} />
+      <CustomForm formId='example-form-123' onSubmit={(data) => console.log(data)} />
     </>
   );
 };
