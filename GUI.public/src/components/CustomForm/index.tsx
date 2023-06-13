c8638ddac4fb37ca5d4106ae0cc5f8b97f11c079
baha-a <bhlshrf@gmail.com>
2023-06-13 15:20:40 +0300

    Add form validations

--- GUI.public/src/components/CustomForm/index.tsx
+++ GUI.public/src/components/CustomForm/index.tsx
@@ -1,29 +1,35 @@
-import React, { FC, useState, useEffect } from 'react';
+import React, { FC, useState, useEffect, FormEvent } from 'react';
 import axios from 'axios';
 import { getCustomFormConfig } from '../../resources/api-constants';
+import Button from '../Button';
 
 interface Field {
   id: string;
   label: string;
   type: 'text' | 'checkbox' | 'select' | 'email' | 'number' | 'date';
   options?: { value: string; label: string }[];
+  vaildationType?: string;
 }
 
 interface FormData {
+  version: string;
+  vaildatorSource: string;
   fields: Field[];
 }
 
 interface CustomFormProps {
   formId: string;
+  onSubmit: (formData: any) => void;
 }
 
-const CustomForm: FC<CustomFormProps> = ({ formId }: CustomFormProps) => {
+const CustomForm: FC<CustomFormProps> = ({ formId, onSubmit }: CustomFormProps) => {
   const [formData, setFormData] = useState<FormData | null>(null);
   const [error, setError] = useState<boolean>(false);
+  const [formValues, setFormValues] = useState<{ [key: string]: string }>({});
 
   useEffect(() => {
     fetchFormData();
-  }, []);
+  }, [formId]);
 
   const fetchFormData = async () => {
     try {
@@ -35,64 +41,107 @@ const CustomForm: FC<CustomFormProps> = ({ formId }: CustomFormProps) => {
     }
   }
 
-  if (!formData) {
-    return (
-      <div>Loading...</div>
-    );
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    onSubmit({ formId, values: formValues });
+  }
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    let { name, value } = e.target;
+    if (e.target.type === 'checkbox') {
+      value = value === 'on' ? 'off' : 'on';
+    }
+    setFormValues((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
   }
 
   if (error) {
     return (
       <div>
         <span>Failed to fetch the required form</span>
-        <button onClick={() => fetchFormData()}>retry</button>
+        <Button appearance='primary' onClick={() => fetchFormData()}>retry</Button>
       </div>
     )
   }
 
+  if (!formData) {
+    return (
+      <div>Loading...</div>
+    );
+  }
+
   return (
     <div>
-      {formData.fields.map((field: any) => {
-        switch (field.type) {
-          case 'text':
-          case 'email':
-          case 'number':
-            return (
-              <div key={field.id}>
-                <label htmlFor={field.id}>{field.label}</label>
-                <input type={field.type} id={field.id} name={field.id} />
-              </div>
-            );
-          case 'checkbox':
-            return (
-              <div key={field.id}>
-                <label htmlFor={field.id}>{field.label}</label>
-                <input type="checkbox" id={field.id} name={field.id} />
-              </div>
-            );
-          case 'select':
-            return (
-              <div key={field.id}>
-                <label htmlFor={field.id}>{field.label}</label>
-                <select id={field.id} name={field.id}>
-                  {field.options?.map(option => (
-                    <option key={option.value} value={option.value}>{option.label}</option>
-                  ))}
-                </select>
-              </div>
-            );
-          case 'date':
-            return (
-              <div key={field.id}>
-                <label htmlFor={field.id}>{field.label}</label>
-                <input type="date" id={field.id} name={field.id} />
-              </div>
-            );
-
-          default:
-            return null;
-        }
-      })}
+      <form onSubmit={handleSubmit}>
+        {formData.fields.map((field: any) => {
+          switch (field.type) {
+            case 'text':
+            case 'email':
+            case 'number':
+              return (
+                <div key={field.id}>
+                  <label htmlFor={field.id}>{field.label}</label>
+                  <input
+                    type={field.type}
+                    id={field.id}
+                    name={field.id}
+                    value={formValues[field.id] || ''}
+                    onChange={handleInputChange}
+                  />
+                </div>
+              );
+            case 'checkbox':
+              return (
+                <div key={field.id}>
+                  <label htmlFor={field.id}>{field.label}</label>
+                  <input
+                    type="checkbox"
+                    id={field.id}
+                    name={field.id}
+                    checked={formValues[field.id] === 'on'}
+                    onChange={handleInputChange}
+                  />
+                </div>
+              );
+            case 'date':
+              return (
+                <div key={field.id}>
+                  <label htmlFor={field.id}>{field.label}</label>
+                  <input
+                    type="date"
+                    id={field.id}
+                    name={field.id}
+                    value={formValues[field.id] || ''}
+                    onChange={handleInputChange}
+                  />
+                </div>
+              );
+            case 'select':
+              return (
+                <div key={field.id}>
+                  <label htmlFor={field.id}>{field.label}</label>
+                  <select
+                    id={field.id}
+                    name={field.id}
+                    value={formValues[field.id] || ''}
+                    onChange={handleInputChange}
+                  >
+                    {field.options?.map((option: any) => (
+                      <option key={option.value} value={option.value}>
+                        {option.label}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+              );
+            default:
+              return null;
+          }
+        })}
+        <Button type='submit'>submit</Button>
+      </form>
     </div>
   )
 }
--- GUI.public/src/pages/OverviewPage.tsx
+++ GUI.public/src/pages/OverviewPage.tsx
@@ -3,45 +3,17 @@ import axios from 'axios';
 import { useTranslation } from 'react-i18next';
 import { Button, Track } from '../components';
 import { dummyApi, toggleDummyApi } from '../resources/api-constants';
+import CustomForm from '../components/CustomForm';
 
 const OverviewPage: React.FC = () => {
   const { t } = useTranslation();
-  const [result, setResult] = useState('');
-
-  useEffect(() => {
-    fetchDummy();
-  }, []);
-
-  const fetchDummy = async () => {
-    try {
-      setResult('loading . . . .');
-      const res = await axios.get(dummyApi());
-      setResult(JSON.stringify(res.data));
-    } catch (err: any) {
-      setResult('failed to fetch: ' + err.toString());
-    }
-  };
-
-  const handleToggleClick = async () => {
-    try {
-      setResult('loading . . . .');
-      await axios.post(toggleDummyApi());
-      await fetchDummy();
-    } catch (err: any) {
-      setResult('failed to toggle: ' + err.toString());
-    }
-  };
 
   return (
     <>
       <Track justify="between">
         <h1>{t('overview.title')}</h1>
       </Track>
-
-      <Track gap={20}>
-        <span>{result}</span>
-        <Button onClick={handleToggleClick}>toggle</Button>
-      </Track>
+      <CustomForm formId='1234' onSubmit={(data) => console.log(data)} />
     </>
   );
 };
