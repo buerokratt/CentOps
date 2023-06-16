8d37cd399b2910adb07accec6a367b8bdb598a19
Karl Jõgi <32520242+karljogi@users.noreply.github.com>
2023-06-16 16:20:54 +0300

    Fix issues from merge

--- DSL/Resql/centops/create-participant-application.sql
+++ DSL/Resql/centops/create-participant-application.sql
@@ -1,3 +1,3 @@
 INSERT INTO participants(name, institution_id, contact_email, name_abbreviated, ip_address, host)
-VALUES (:name, :institution_id, :contact_email, :name_abbreviated, :ip_address::CIDR, :host);
+VALUES (:name, :institution_id, :contact_email, :name_abbreviated, :ip_address::CIDR, :host)
 RETURNING id, unique_identifier;
