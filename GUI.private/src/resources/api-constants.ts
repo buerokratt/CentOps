909a7539fecc265f0e304a61d67e796aa400a22d
baha-a <bhlshrf@gmail.com>
2023-06-19 11:42:54 +0300

    Remove dummy example from code

--- GUI.private/src/resources/api-constants.ts
+++ GUI.private/src/resources/api-constants.ts
@@ -2,11 +2,9 @@ const baseUrl = process.env.REACT_APP_API_URL;
 
 export const createInvitation = (): string =>
   `${baseUrl}/admin/participants/invitation`;
-export const dummyApi = (): string => `${baseUrl}/dummy`;
 export const updateParticipantDetails = (uuid: string): string =>
   `${baseUrl}/admin/participants?uuid=${uuid}`;
 export const updateParticipantStatus = (uuid: string): string =>
   `${baseUrl}/admin/participants/status?uuid=${uuid}`;
 export const deleteParticipant = (uuid: string): string =>
   `${baseUrl}/admin/participants?uuid=${uuid}`;
-export const toggleDummyApi = (): string => `${baseUrl}/toggle-dummy`;
