ffcf181170e093cdcd98f8d0dbdbfbaae2fe6d42
ahmedyasser <ahmedyasser.dev@hotmail.com>
2023-06-20 03:19:17 +0300

    Updated Update Table Liquibase

--- DSL/Liquibase/changelog/1687150680-create-table-manifests-updates.sql
+++ DSL/Liquibase/changelog/1687150680-create-table-manifests-updates.sql
@@ -1,5 +1,5 @@
 -- liquibase formatted sql
--- changeset ahmedyasser:1687150678
+-- changeset ahmedyasser:1687150680
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE EXTENSION IF NOT EXISTS "hstore";
@@ -7,6 +7,7 @@ CREATE EXTENSION IF NOT EXISTS "hstore";
 CREATE TABLE manifests_updates (
   id BIGSERIAL PRIMARY KEY,
   parent_manifest_id uuid NOT NULL REFERENCES manifests (manifest_id),
+  update_id uuid UNIQUE NOT NULL DEFAULT uuid_generate_v4 (),
   buerokratt_version TEXT NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT now(),
   updated_at TIMESTAMP DEFAULT NULL,
