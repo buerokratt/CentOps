894b9482464a28ada10a17979b75d73801fd1236
ahmedyasser <ahmedyasser.dev@hotmail.com>
2023-06-26 20:39:02 +0300

    Added Changes to Manifests History

--- GUI.private/src/pages/manifests/ManifestsHistoryPage.tsx
+++ GUI.private/src/pages/manifests/ManifestsHistoryPage.tsx
@@ -1,6 +1,40 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { ManifestHistory } from '../../types/manifestHistory';
+import { useQuery } from '@tanstack/react-query';
+import { DataTable, Label } from '../../components';
+import { createColumnHelper } from '@tanstack/react-table';
 
 const ManifestsHistoryPage: React.FC = () => {
-  return <></>;
+  const { t } = useTranslation();
+
+  const { data: history } = useQuery<ManifestHistory[]>({
+    queryKey: ['manifest/history'],
+    onSuccess: (data) => {
+      console.log(data);
+      console.log(history);
+    },
+  });
+
+  const appRequestColumnHelper = createColumnHelper<ManifestHistory>();
+  const appRequestColumns = useMemo(
+    () => [
+      appRequestColumnHelper.accessor('historyId', {
+        header: 'History ID',
+        cell: (uniqueIdentifier) => uniqueIdentifier.getValue(),
+      }),
+    ],
+    [history, appRequestColumnHelper, t]
+  );
+
+  return (
+    <>
+      <DataTable data={history} columns={appRequestColumns} />
+      {/* {history && history.length && (
+        <DataTable data={history} columns={appRequestColumns} />
+      )} */}
+    </>
+  );
 };
 export default ManifestsHistoryPage;
---
+++ GUI.private/src/types/manifestHistory.ts
@@ -0,0 +1,3 @@
+export interface ManifestHistory {
+  readonly historyId: string;
+}
