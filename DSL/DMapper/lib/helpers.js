91f289863f269bf6b0138c4f585f63e5ce82a818
baha-a <bhlshrf@gmail.com>
2023-06-27 16:33:40 +0300

    Use encryption for message content

--- DSL/DMapper/lib/helpers.js
+++ DSL/DMapper/lib/helpers.js
@@ -1,6 +1,11 @@
-export function sayHello() {
-  return "hello there!!!";
+import aesDecrypt from "../js/decryption/aes.js";
+
+const ENCRYPTION_KEY = 1234;
+
+export function encryptMessage(message) {
+  return aesEncrypt(message, ENCRYPTION_KEY);
 }
+
 export function decryptMessage(encryptedMessage) {
-  return encryptedMessage;
+  return aesDecrypt(encryptedMessage, ENCRYPTION_KEY, false);
 }
