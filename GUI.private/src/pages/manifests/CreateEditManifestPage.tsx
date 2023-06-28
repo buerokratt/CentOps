d67a02255d2b91baf872bb9ba7e42af04b661805
ahmedyasser <ahmedyasser.dev@hotmail.com>
2023-06-28 09:56:55 +0300

    Added Update Manifest Update

--- GUI.private/src/pages/manifests/CreateEditManifestPage.tsx
+++ GUI.private/src/pages/manifests/CreateEditManifestPage.tsx
@@ -1,6 +1,32 @@
 import React, { useState } from 'react';
+import {
+  ReactFormGenerator,
+  ElementStore,
+  ReactFormBuilder,
+} from 'react-form-builder2';
 
 const CreateEditManifestPage: React.FC = () => {
-  return <></>;
+  var items = [
+    {
+      key: 'Header',
+      name: 'Header Text',
+      icon: 'fa fa-header',
+      static: true,
+      content: 'Placeholder Text...',
+    },
+    {
+      key: 'Paragraph',
+      name: 'Paragraph',
+      static: true,
+      icon: 'fa fa-paragraph',
+      content: 'Placeholder Text...',
+    },
+  ];
+
+  return (
+    <>
+      <ReactFormBuilder />
+    </>
+  );
 };
 export default CreateEditManifestPage;
