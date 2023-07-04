7c8c9d33c217c0340ddf5e20a62b56ff04a9d7fa
Ahmed yasser <ahmedyasser.dev@hotmail.com>
2023-07-04 15:53:42 +0300

    27 An Overview Of Bürokratt's Future Updates (#125)
    * chore(cent-ops): added create manifest yml
    
    * fix(cent-ops): removed teck stack
    
    * fix(cent-ops): removed teck stack
    
    * chore(cent-ops): added confgurations sql -yml
    chore(cent-ops): added user login sql - yml
    chore(cent-ops): renamed resql users container
    
    * Renamed Datamapper Image
    
    * Added users seeder
    
    * Added User Login
    
    * Added Manifest Base Operations
    
    * Updated Update Table Liquibase
    
    * Enhanced Manifest Base Operations
    
    * Enhanced Update Operations
    
    * Added Manifests Getters
    
    * Added Manifest Getters Endpoints
    
    * Added DMR Mock Endpoints
    
    * Added Approved Manifests
    
    * Added Base Manifests GUI Pages
    
    * Added Changes to Manifests History
    
    * Renamed Manifest Type
    
    * Removed unused imports & logs
    
    * Added Overview Page
    Added New Manifests Page
    Added Manifests Updates Page
    
    * Added Update Manifest Update
    
    * Enhanced Details Page
    
    * Added Delete Manifest
    
    * Added New Manifest Configurations
    
    * Added Lottie
    
    * Modified Delete Functionality
    
    * Fixed Merge Errors
    
    * Added Create Edit Manifest Page
    
    * Modified Create Edit Manifest Page
    
    * Added Edit Update
    
    * Added Past Updates
    
    * Added Future Updates
    
    * Synced Main

--- GUI.public/src/RootComponent.tsx
+++ GUI.public/src/RootComponent.tsx
@@ -12,6 +12,7 @@ import { ROUTES } from './resources/routes-constants';
 import './styles/main.scss';
 import ManifestsDetailsPage from './pages/manifests/ManifestsDetailsPage';
 import PastUpdatesPage from './pages/manifests/PastUpdatesPage';
+import FutureUpdatesPage from './pages/manifests/FutureUpdatesPage';
 
 const RootComponent: React.FC = () => {
   return (
@@ -31,8 +32,13 @@ const RootComponent: React.FC = () => {
           path={ROUTES.MANIFESTS_PAST_UPDATES_ROUTE}
           element={<PastUpdatesPage />}
         />
-
+        
         <Route
+          path={ROUTES.MANIFESTS_FUTURE_UPDATES_ROUTE}
+          element={<FutureUpdatesPage />}
+        />
+
+        <Route    
           path={ROUTES.MANIFESTS_DETAILS_ROUTE}
           element={<ManifestsDetailsPage />}
         />
