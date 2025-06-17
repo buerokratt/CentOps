c194ebfab6095f38bfa1343d8f853db327579cbe
turkeshintroduct <turkesh.gozalov@introduct.tech>
2025-06-17 21:11:04 +0400

    Implement logging vault accesses (#262)
    * logging vault accesses
    
    * modified docker compose file and DSL files
    
    * files location changed

---
+++ DSL/Resql/centops/POST/log-vault-api-get-delete-error-action.sql
@@ -0,0 +1,14 @@
+INSERT INTO vault_api_action_log (client_id,
+                                  ip_address,
+                                  action,
+                                  secret_key,
+                                  status_code,
+                                  error_message,
+                                  user_agent)
+VALUES (:client_id,
+        :ip_address,
+        :action,
+        :secret_key,
+        :status_code,
+        :error_message,
+        :user_agent) RETURNING id, timestamp;
