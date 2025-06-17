80a1fa42529b4732f56f721dee8028f21dfa67bf
Dmitry Efimenko <efim4eg@gmail.com>
2025-06-17 15:41:06 +0300

    Implement GUI for viewing documentation (#289)

--- GUI/src/i18n/trans/nav.tsx
+++ GUI/src/i18n/trans/nav.tsx
@@ -4,6 +4,7 @@ import { Trans } from 'react-i18next';
 export const TransNav = createTrans({
   clients: <Trans i18nKey="nav.clients" defaults="Clients" />,
   clusters: <Trans i18nKey="nav.clusters" defaults="Clusters" />,
+  documentation: <Trans i18nKey="nav.documentation" defaults="Documentation" />,
   settings: <Trans i18nKey="nav.settings" defaults="Settings" />,
   users: <Trans i18nKey="nav.users" defaults="Users" />,
 });
