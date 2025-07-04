22400ef5bf5b9006b2deb2dc5bc390cfffeb9d9c
turkeshintroduct <turkesh.gozalov@introduct.tech>
2025-07-04 13:38:10 +0200

    removed ruuter public, modified DSLs

--- DSL/Resql/centops/POST/update-documentation.sql
+++ DSL/Resql/centops/POST/update-documentation.sql
@@ -1,5 +1,9 @@
-INSERT INTO documentations (document_id, content, updated_at)
-SELECT document_id, :content, now()
+INSERT INTO documentations (document_id, content, created_at, updated_at)
+SELECT
+    document_id,
+    :content,
+    created_at,
+    now()
 FROM documentations
 WHERE document_id = :document_id::uuid
   AND id = (SELECT max(id) FROM documentations WHERE document_id = :document_id::uuid);
