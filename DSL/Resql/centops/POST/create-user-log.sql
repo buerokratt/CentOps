c19d62cfc41e7e3778b2b55cf6e8c0633e898160
bodax <markpolo525@gmail.com>
2025-07-05 17:25:34 +0200

    feat(271): Add user logs to manifests

--- DSL/Resql/centops/POST/create-user-log.sql
+++ DSL/Resql/centops/POST/create-user-log.sql
@@ -1,2 +1,2 @@
-INSERT INTO user_logs (method, path)
-VALUES (:method, :path);
+INSERT INTO user_logs (method, path, user_id_code, first_name, last_name)
+VALUES (:method, :path, :user_id_code, :first_name, :last_name);
