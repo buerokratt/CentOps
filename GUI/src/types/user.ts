2a42269c5aacc1d2a8b9a9308f41c7ae897334d2
borm <efim4eg@gmail.com>
2025-07-08 12:50:10 +0300

    feat(273): Implement GUI for user list / BE integration

--- GUI/src/types/user.ts
+++ GUI/src/types/user.ts
@@ -1,4 +1,13 @@
 export interface User {
   id: string;
+  userId: string;
   name: string;
+  firstName: string;
+  lastName: string;
+  identificationNo: string;
+}
+
+export interface ApiUser extends User {
+  createdAt: string;
+  updatedAt: string;
 }
