d442b40af1a9cf7534f687688ee33975c09c2368
1AhmedYasser <26207361+1AhmedYasser@users.noreply.github.com>
2025-07-17 11:03:58 +0300

    fix(0): Changed GUI Auth Path

--- GUI/src/dev.ts
+++ GUI/src/dev.ts
@@ -1,5 +1,5 @@
 export async function auth() {
-  const res = await fetch(`${import.meta.env.REACT_APP_API_URL}/login-user`, {
+  const res = await fetch(`${import.meta.env.REACT_APP_API_URL}/auth/login`, {
     headers: { 'Content-Type': 'application/json' },
     method: 'POST',
     body: JSON.stringify({ login: 'EE30303039914', password: 'OK' }),
