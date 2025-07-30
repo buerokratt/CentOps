8bf9cba543aaf8de9953dd8c60bdaa9f5deba7b8
bohdanintro <211696835+bohdanintro@users.noreply.github.com>
2025-07-31 00:28:31 +0200

    feat(3): Add testcontainers and tests

--- tests/e2e-test/src/setup/test-config.js
+++ tests/e2e-test/src/setup/test-config.js
@@ -1,7 +1,5 @@
-import dotenv from 'dotenv';
-dotenv.config();
 
 export const headers = {
         'Content-Type': 'application/json',
-        'cookie': process.env.COOKIE_VALUE
+        'cookie': "mockCookieValue"
 };
