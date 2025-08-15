cc16dc2b16ce4a0e373dac71a4be83516f48d936
turkeshintroduct <turkesh.gozalov@introduct.tech>
2025-08-15 12:22:21 +0200

    added ip address to user logs for clients actions

--- DSL/Resql/centops/POST/create-user-log.sql
+++ DSL/Resql/centops/POST/create-user-log.sql
@@ -1,2 +1,2 @@
-INSERT INTO user_logs (method, path, user_id_code, first_name, last_name)
-VALUES (:method, :path, :user_id_code, :first_name, :last_name);
+INSERT INTO user_logs (method, path, user_id_code, first_name, last_name, ip_address)
+VALUES (:method, :path, :user_id_code, :first_name, :last_name, :ip_address);
