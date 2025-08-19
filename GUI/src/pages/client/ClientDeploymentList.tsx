39874fababe0e99ef2bb46a6ac41a8c87b870013
borm <efim4eg@gmail.com>
2025-08-19 15:25:11 +0300

    fix(454): Navigation logic fix

--- GUI/src/pages/client/ClientDeploymentList.tsx
+++ GUI/src/pages/client/ClientDeploymentList.tsx
@@ -20,7 +20,7 @@ import { createColumnHelper, type SortingState } from '@tanstack/react-table';
 import { TransButton } from 'i18n/trans/button';
 import { Trans } from 'react-i18next';
 import { TransTableHead } from 'i18n/trans/table';
-import { Link } from 'components/Router/Link';
+import { Link, replaceLinkParams } from 'components/Router/Link';
 import { ROUTES } from 'resources/routes-constants';
 import { TransTitle } from 'i18n/trans/title';
 import { TransLabel } from 'i18n/trans/label';
@@ -112,6 +112,11 @@ export const ClientDeploymentList = withAuthorization(() => {
           <Link
             to={ROUTES.CLIENT_MANIFESTS_DETAILS_ROUTE}
             params={{ manifestId: message.row.original.manifestId }}
+            state={{
+              backUrl: replaceLinkParams(ROUTES.CLIENT_DEPLOYMENTS_ROUTE, {
+                clientId,
+              }),
+            }}
           >
             {message.getValue()}
           </Link>
