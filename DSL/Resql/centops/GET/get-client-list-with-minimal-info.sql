59c6c51792c7a3d6221a3de756f92537b10d932b
Igor Krupenja <igor.krupenja@gmail.com>
2025-12-30 10:01:32 +0200

    feat(1663): add sort and filter

--- DSL/Resql/centops/GET/get-client-list-with-minimal-info.sql
+++ DSL/Resql/centops/GET/get-client-list-with-minimal-info.sql
@@ -1,12 +1,36 @@
-SELECT client_id,
-       name,
-       authentication_certificate,
-       created_at,
-       updated_at,
-       CEIL(COUNT(*) OVER() / :page_size::DECIMAL) AS total_pages
+SELECT
+    client_id,
+    name,
+    authentication_certificate,
+    created_at,
+    updated_at,
+    CEIL(
+        COUNT(*) OVER () /:page_size::DECIMAL
+    ) AS total_pages
 FROM clients c
-WHERE id = (SELECT max(id) FROM clients WHERE client_id = c.client_id)
-  AND deleted = FALSE
-  AND part_of_network = TRUE
-ORDER BY name
-OFFSET ((GREATEST(:page::INTEGER, 1) - 1) * :page_size::INTEGER ) LIMIT :page_size::INTEGER;
+WHERE
+    id = (
+        SELECT max(id)
+        FROM clients
+        WHERE
+            client_id = c.client_id
+    )
+    AND deleted = FALSE
+    AND part_of_network = TRUE
+    AND (
+:filter_by_name = ''
+        OR name ILIKE '%' ||:filter_by_name || '%'
+    )
+ORDER BY
+    CASE
+        WHEN:sort_order = 'desc' THEN name
+    END DESC,
+    CASE
+        WHEN:sort_order = 'asc' THEN name
+    END ASC
+OFFSET (
+        (
+            GREATEST(:page::INTEGER, 1) - 1
+        ) *:page_size::INTEGER
+    )
+LIMIT:page_size::INTEGER;
